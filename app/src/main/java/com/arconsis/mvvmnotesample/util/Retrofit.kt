bdb98d0ba8f549dcd22617eabf50c9a80767d21b
Alexander Frank <alexander.frank@arconsis.com>
2017-05-15 18:02:17 +0200

    Moved hard coded server ip into config

--- app/src/main/java/com/arconsis/mvvmnotesample/util/Retrofit.kt
+++ app/src/main/java/com/arconsis/mvvmnotesample/util/Retrofit.kt
@@ -1,5 +1,7 @@
 package com.arconsis.mvvmnotesample.util
 
+import android.util.Log
+import com.arconsis.mvvmnotesample.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -7,8 +9,9 @@ import retrofit2.converter.gson.GsonConverterFactory
  * Created by Alexander on 05.05.2017.
  */
 fun retrofit(): Retrofit {
+    val url = BuildConfig.SERVER_URL + "api/note/"
     return Retrofit.Builder()
-            .baseUrl("http://192.168.2.190:8080/mvvm-note-backend-1.0-SNAPSHOT/api/note/")
+            .baseUrl(url)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 }
